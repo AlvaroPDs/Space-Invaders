439036ec7f8601ac57dfd41ee540499d803f8caf
Alvaro <75389804+AlvaroPDs@users.noreply.github.com>
2025-05-19 14:21:47 +0000

    up 1

---
+++ main.c
@@ -0,0 +1,13 @@
+/* Copyright (C) 2025 Alvaro Palma Da Silva - All Rights Reserved
+
+ * Licensed under the GNU GPL V3.0 license. All conditions apply.
+
+ * Powered by Allegro: http://liballeg.org/license.html
+
+ */
+
+ #include <stdio.h>
+ #include <allegro5/allegro.h>
+ #include <allegro5/allegro_images.h>
+
+ const float FPS = 90;
