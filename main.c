62dd89d6ec8f5fe03c6093230c32e8ed6afb1a31
Alvaro <75389804+AlvaroPDs@users.noreply.github.com>
2025-05-26 14:59:17 +0000

    up 2

--- main.c
+++ main.c
@@ -6,21 +6,43 @@
 
  */
 
- #include <stdio.h>
- #include <allegro5/allegro.h>
- #include <allegro5/allegro_images.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <allegro5/allegro.h>
+#include <allegro5/allegro_image.h>
+
 
  const float FPS = 90;
  const int SCREEN_W = 1024;
  const int SCREEN_H = 720;
 
- int main(int argc, char***argv)
+ int main(int argc, char **argv)
  {
     ALLEGRO_DISPLAY *display = NULL;
     ALLEGRO_EVENT_QUEUE *event_queue = NULL;
-    ALLEGRO_TIMER *timer = NULL; 
+    //ALLEGRO_TIMER *timer = NULL;
 
+    if(!al_init())
+    {
+        fprintf(stderr, "failed to initialize allegro!\n");
+        return -1;
 
+    }
+    //timer = al_create_timer(1.0/FPS);
+    //if(!timer)
+    //{
+    //    printf(stderr,("failed to create timer!\n"))
+    //}
+    bool redraw = true;
+
+    al_set_new_display_flags(ALLEGRO_OPENGL | ALLEGRO_WINDOWED);
+    display = al_create_display(SCREEN_W, SCREEN_H);
+    if(!display)
+    {
+        fprintf(stderr,"failed to create display!\n");
+        //al_destroy_timer(timer);
+        return -1;
+    }
 
 return 0;
 
